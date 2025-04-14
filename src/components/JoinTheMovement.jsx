16c5aa00482f96bf1f0e53c9307b2ee183e83c0b
47gfx <117771075+4700gfx@users.noreply.github.com>
2025-04-13 21:22:07 -0400

    Adding Components

---
+++ src/components/JoinTheMovement.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const JoinTheMovement = () => {
+  return (
+    <div>JoinTheMovement</div>
+  )
+}
+
+export default JoinTheMovement
---
+++ src/components/WhatsComing.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const WhatsComing = () => {
+  return (
+    <div>WhatsComing</div>
+  )
+}
+
+export default WhatsComing
